eba8ecf2c749111c90ed3223273643bdcf91b908
马如林 <marulin@ceair.com>
2018-03-09 22:31:19 +0800

    hello world

---
+++ dubbo-demo-provider/src/main/java/com/alibaba/dubbo/demo/Provider.java
@@ -0,0 +1,13 @@
+package com.alibaba.dubbo.demo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Provider {
+  public static void main(String[] args) throws Exception {
+    ClassPathXmlApplicationContext context =
+        new ClassPathXmlApplicationContext(new String[] {"META-INF/spring/dubbo-demo-provider.xml"});
+    context.start();
+    // press any key to exit
+    System.in.read();
+  }
+}
